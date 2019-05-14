0bcbba082cf1d9528675a4cda31e66281cf90835
bostdiek <bostdiek@gmail.com>
2019-05-14 14:50:42 -0700

    download raw data works

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,18 +2,50 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+# from dotenv import find_dotenv, load_dotenv
+from subprocess import call
+
+project_dir = Path(__file__).resolve().parents[2]
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+def download_raw_data():
+    logger = logging.getLogger(__name__)
+
+    googled = 'https://drive.google.com/file/d/'
+    bkg = googled + '1iYu3vTcNcF1Gg5NplW4WmUkM0I2Jto80/view?usp=sharing'
+    p2 = googled + '1ShExpnRkDBkKn6SVxTYTCtc9HNjydmfL/view?usp=sharing'
+    p3 = googled + '11dWNfgtRWJqbg0uRdyOM1yvHByh1Q3gf/view?usp=sharing'
+    p4 = googled + '1mr1_0OmLfCRI-eude6gWI9RjObvFX7jZ/view?usp=sharing'
+    remotes = [bkg, p2, p3, p4]
+
+    basedr = 'data/raw/'
+    filenames = ['data_bkg.txt',
+                 'data_sig_2p.txt',
+                 'data_sig_3p.txt',
+                 'data_sig_4p.txt'
+                 ]
+    for loc_file, remote_file in zip(filenames, remotes):
+        raw_name = project_dir.joinpath(basedr + loc_file)
+        raw_str = raw_name.resolve()
+        if raw_name.exists():
+            logger.info('The file {0} has already been downloaded'.format(raw_str))
+        else:
+            logger.info('Downloading {0}'.format(raw_str))
+            call('curl -o {0} {1}'.format(raw_str, remote_file),
+                 shell=True)
+            logger.info('Download finished')
+
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    download_raw_data()
 
 
 if __name__ == '__main__':
@@ -25,6 +57,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    # load_dotenv(find_dotenv())
 
     main()
