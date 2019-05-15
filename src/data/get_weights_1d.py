54f6e4abfdcfd6fb32a3e2aa982675c93cd2cfce
bostdiek <bostdiek@gmail.com>
2019-05-14 23:23:15 -0700

    save planing weights

--- src/data/get_weights_1d.py
+++ src/data/get_weights_1d.py
@@ -6,6 +6,7 @@ Computes the paning weights to flatten the mass distribtion
 import numpy as np
 import pandas as pd
 from scipy import interpolate
+import logging
 
 
 def load_data(prong):
@@ -69,38 +70,84 @@ def get_weights(x, interp):
     probs = interp(x)  # get value of each bin with interpolating function
     weights = probs.sum() / probs  # normalize weights
     weights /= weights.mean()  # normalize weights
-    weights = weights.reshape(-1, 1)
+    weights = weights
     return weights
 
 
 def set_weights(prong):
+    logger = logging.getLogger(__name__)
+    logger.info('Setting weights for {0}p prong'.format(prong))
+
     data = load_data(prong)
     m_train, y_train = data[0]
     m_test, y_test = data[1]
     m_val, y_val = data[2]
 
+    # print(m_train.shape)
     min_mass, max_mass = np.min(m_train), np.max(m_train)
     #  set the used min and max to 5% of the difference larger and smaller
     diff = max_mass - min_mass
     min_mass, max_mass = min_mass - 0.05 * diff, max_mass + 0.05 * diff
 
-    sig_mass = m_train[y_train == 1]
+    sig_mass = m_train[np.ravel(y_train == 1)]
+    # print(sig_mass.shape)
     sig_probs = get_probs_1d(x=sig_mass,
                              min_val=min_mass,
                              max_val=max_mass,
                              bins=100)
 
-    back_mass = m_train[y_train == 0]
-    back_probs = get_probs_1d(x=sig_mass,
+    back_mass = m_train[np.ravel(y_train == 0)]
+    back_probs = get_probs_1d(x=back_mass,
                               min_val=min_mass,
                               max_val=max_mass,
                               bins=100)
 
     # new array for the output weights
-    train_weights = np.zeros_like
+    train_weights = np.zeros_like(m_train)
+    train_weights[np.ravel(y_train == 1)] = get_weights(x=sig_mass,
+                                                        interp=sig_probs
+                                                        )
+    train_weights[np.ravel(y_train == 0)] = get_weights(x=back_mass,
+                                                        interp=back_probs
+                                                        )
+    np.save('data/interim/train_planing_weights_{0}p.npy'.format(prong),
+            train_weights.reshape(-1, 1)
+            )
+
+    #  test data
+    test_weights = np.zeros_like(m_test)
+    sigs = np.ravel(y_test == 1)
+    backs = np.ravel(y_test == 0)
+    test_weights[sigs] = get_weights(x=m_test[sigs],
+                                     interp=sig_probs
+                                     )
+    test_weights[backs] = get_weights(x=m_test[backs],
+                                      interp=back_probs
+                                      )
+    # print(test_weights)
+    np.save('data/interim/test_planing_weights_{0}p.npy'.format(prong),
+            test_weights
+            )
+
+    #  test data
+    valweights = np.zeros_like(m_val)
+    sigs = np.ravel(y_val == 1)
+    backs = np.ravel(y_val == 0)
+    valweights[sigs] = get_weights(x=m_val[sigs],
+                                   interp=sig_probs
+                                   )
+    valweights[backs] = get_weights(x=m_val[backs],
+                                    interp=back_probs
+                                    )
+    # print(valweights)
+    np.save('data/interim/val_planing_weights_{0}p.npy'.format(prong),
+            valweights
+            )
+    logger.info('saved weights for {0}p prong'.format(prong))
 
 
-print('getting weights')
-sig_weights = get_weights_1d(sig_mass,min_val=75,max_val=325,bins=100)
-back_weights = get_weights_1d(back_mass,min_val=75,max_val=325,bins=100)
-print('done getting weights')
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    set_weights(2)
