d67ab1024a48be10a07c5559b08a59050743e957
Bryan Ostdiek <bostdiek@uoregon.edu>
2019-05-19 19:56:33 -0700

    Model trained on talapas

---
+++ LaunchScan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=2p    ### Job Name
+#SBATCH --partition=gpu       ### Quality of Service (like a queue in PBS)
+#SBATCH --time=0-18:00:00     ### Wall clock time limit in Days-HH:MM:SS
+#SBATCH --nodes=1             ### Node count required for the job
+#SBATCH --ntasks-per-node=7   ### Nuber of tasks to be launched per Node
+#SBATCH --gres=gpu:1          ### General REServation of gpu:number of gpus
+#SBATCH --mem=32G             ### Memory
+#SBATCH --array=2,5,20,50,200,500           ### Arrays for lambda
+#SBATCH --output=SlurmOuts/output_2p_%a.txt
+#SBATCH --error=SlurmErrors/error_2p_%a.txt
+#SBATCH --mail-user=bostdiek@uoregon.edu ### email for alerts
+#SBATCH --mail-type=ALL
+
+PRONG=2
+
+module load cuda/9.0
+module load python3
+
+echo $SLURM_NTASKS
+
+export OMP_NUM_THREADS=$SLURM_NTASKS
+
+cd /projects/het/bostdiek/MassAgnostic-JetTaggers
+
+python3 src/models/train_Adversarial.py --prong=$PRONG --lam=${SLURM_ARRAY_TASK_ID}
