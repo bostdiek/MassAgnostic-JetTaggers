0f3b11057d6d1b41a267503e5c2a034ddab78258
bostdiek <bostdiek@gmail.com>
2019-05-24 04:07:57 -0700

    uboost and gbc preditions

--- src/models/predict_models.py
+++ src/models/predict_models.py
@@ -3,9 +3,10 @@
 Author: Bryan Ostdiek (bostdiek@gmail.com)
 This file will reads data from the data/interm directory
 Loads models previously saved
-Makes predictions and saves them to the modelpredictions directory
+Makes predictions and saves them to the data/modelpredictions directory
 '''
 import click
+from collections import OrderedDict
 import joblib
 import numpy as np
 import pandas as pd
@@ -15,15 +16,71 @@ import pickle
 from pathlib import Path
 import logging
 
-from HelperFunctions import load_data_bdt
+from HelperFunctions import load_data_bdt, write_roc_pickle, make_histos
 
 
-def predict_bdts(prong):
+def predict_uboost(prong):
     '''
+    Computes the predictions for the uBoost method.
+    Inputs:
+        prong: interger denoting the number of prongs in the signal jets
+    Outputs:
+        sig_prob: [np array] probability of being selected for each event
+        fpr: [np array] false positive rate
+        tpr: [np array] true postive rate
+        threholds: [np array] cut values corresponding to the fpr and tpr
+        auc: [float] the area under the ROC curve
     '''
     testdf, y_test, data_cols = load_data_bdt(prong, set='test')
+    jet_mass = testdf[data_cols[0]].values
     name = str(project_dir) + '/models/uBoost_{0}p.p'.format(prong)
+
+    # load the model and make predictions
     uboost = joblib.load(name)
+    sig_prob = uboost.predict_proba(testdf)[:, 1]
+
+    # compute the test statistics
+    fpr_u, tpr_u, thresholds_u = roc_curve(y_true=y_test, y_score=sig_prob)
+    auc_u = auc(fpr_u, tpr_u)
+    roc_info = fpr_u, tpr_u, thresholds_u, auc_u
+    # save the info
+    model_name = 'uBoost_{0}p'.format(prong)
+    write_roc_pickle(model_name, roc_info)
+    histos = make_histos(model_name, jet_mass, sig_prob, y_test, roc_info)
+
+    return histos
+
+
+def predict_gbc(prong):
+    '''
+    Computes the predictions for the gradient boosted decision tree.
+    Inputs:
+        prong: interger denoting the number of prongs in the signal jets
+    Outputs:
+        sig_prob: [np array] probability of being selected for each event
+        fpr: [np array] false positive rate
+        tpr: [np array] true postive rate
+        threholds: [np array] cut values corresponding to the fpr and tpr
+        auc: [float] the area under the ROC curve
+    '''
+    testdf, y_test, data_cols = load_data_bdt(prong, set='test')
+    jet_mass = testdf[data_cols[0]].values
+    name = str(project_dir) + '/models/GBC_{0}p.p'.format(prong)
+
+    # load the model and make predictions
+    gbc = joblib.load(name)
+    sig_prob = gbc.predict_proba(testdf[data_cols[1:]])[:, 1]
+
+    # compute the test statistics
+    fpr_u, tpr_u, thresholds_u = roc_curve(y_true=y_test, y_score=sig_prob)
+    auc_u = auc(fpr_u, tpr_u)
+    roc_info = fpr_u, tpr_u, thresholds_u, auc_u
+    # save the info
+    model_name = 'GBC_{0}p'.format(prong)
+    write_roc_pickle(model_name, roc_info)
+    histos = make_histos(model_name, jet_mass, sig_prob, y_test, roc_info)
+
+    return histos
 
 
 @click.command()
@@ -32,7 +89,11 @@ def predict_bdts(prong):
 def main(prong):
     '''
     '''
-    predict_bdts(2)
+    print('Making uboost predictions')
+    uboost_hist = predict_uboost(prong)
+
+    print('Making gradient boosted decision tree predictions')
+    gbc_hist = predict_gbc(prong)
 
 
 if __name__ == '__main__':
